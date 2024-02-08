24d3d5605101e5dcf045612e74f3b2562ed53eea
zackmoyal <152685724+zackmoyal@users.noreply.github.com>
2024-02-07 22:59:45 -0800

    Initialize answer

---
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChallengesWithTestsMark8
+{
+    public class ChallengesSet02
+    {
+        public bool CharacterIsALetter(char c)
+        {
+            return char.IsLetter(c);
+        }
+
+        public bool CountOfElementsIsEven(string[] vals)
+        {
+            if(vals.Length % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsNumberEven(int number)
+        {
+            if(number % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsNumberOdd(int num)
+        {
+            if (num % 2 != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public double SumOfMinAndMax(IEnumerable<double> numbers)
+        {
+            if(numbers == null)
+            {
+                return 0;
+            }
+
+            if (numbers.Count() == 0)
+            {
+                return 0;
+            }
+
+            return numbers.Min() + numbers.Max();
+        }
+
+        public int GetLengthOfShortestString(string str1, string str2)
+        {
+            if(str1.Length < str2.Length)
+            {
+                return str1.Length;
+            }
+            else
+            {
+                return str2.Length;
+            }
+        }
+
+        public int Sum(int[] numbers)
+        {
+            if(numbers == null)
+            {
+                return 0;
+            }
+
+            var sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+
+            return sum;
+
+            //return numbers.Sum();
+        }
+
+        public int SumEvens(int[] numbers)
+        {
+            if(numbers == null)
+            {
+                return 0;
+            }
+
+            var evenSum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if(numbers[i] % 2 == 0)
+                {
+                    evenSum += numbers[i];
+                }
+            }
+
+            return evenSum;
+        }
+
+        public bool IsSumOdd(List<int> numbers)
+        {
+            if(numbers == null)
+            {
+                return false;
+            }
+
+            return (numbers.Sum() % 2 != 0);
+        }
+
+        public long CountOfPositiveOddsBelowNumber(long number)
+        {
+            //var count = 0;
+
+            //if(number == 1 || number <= 0)
+            //{
+            //    return 0;
+            //}
+
+            //for (long i = number; i > 0; i--)
+            //{
+            //    if(i % 2 == 0)
+            //    {
+            //        count++;
+            //    }
+            //}
+
+            //return count;
+
+            if(number <= 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return number / 2;
+            }
+        }
+    }
+}
---
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChallengesWithTestsMark8
+{
+    public class ChallengesSet04
+    {
+        public int AddEvenSubtractOdd(int[] numbers)
+        {
+            //var sum = 0;
+
+            //foreach (var num in numbers)
+            //{
+            //    if (num % 2 == 0)
+            //    {
+            //        sum += num;
+            //    }
+
+            //    if (num % 2 != 0)
+            //    {
+            //        sum -= num;
+            //    }
+            //}
+
+            //return sum;
+
+            return numbers.Where(x => x % 2 == 0).Sum() - numbers.Where(x => x % 2 != 0).Sum();
+        }
+
+        public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
+        {
+            var list = new List<int> { str1.Length, str2.Length, str3.Length, str4.Length };
+
+            return list.Min();
+        }
+
+        public int GetSmallestNumber(int number1, int number2, int number3, int number4)
+        {
+            var list = new List<int>() { number1, number2, number3, number4 };
+
+            return list.Min();
+        }
+
+        public void ChangeBusinessNameTo_TrueCoders(Business biz)
+        {
+            biz.Name = "TrueCoders";
+        }
+
+        public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
+        {
+            return (
+                    sideLength1 + sideLength2 > sideLength3 &&
+                    sideLength1 + sideLength3 > sideLength2 &&
+                    sideLength2 + sideLength3 > sideLength1
+                   );
+            
+        }
+
+        public bool IsStringANumber(string input)
+        {
+            var isNumber = double.TryParse(input, out double number);
+
+            return isNumber;
+        }
+
+        public bool MajorityOfElementsInArrayAreNull(object[] objs)
+        {
+            var amount = objs.Length;
+            var majority = (amount / 2) + 1;
+
+            var count = 0;
+
+            foreach (var obj in objs)
+            {
+                if (obj == null)
+                {
+                    count++;
+                }
+            }
+
+            if(count >= majority)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public double AverageEvens(int[] numbers)
+        {
+            double sum = 0;
+            double count = 0;
+
+            if(numbers == null)
+            {
+                return 0;
+            }
+
+            foreach(var num in numbers)
+            {
+                if(num % 2 == 0)
+                {
+                    sum += num; //sum = sum + num
+                    count++;
+                }
+            }
+
+            if(count > 0)
+            {
+                return sum / count;
+            }
+            else
+            {
+                return 0;
+            }
+            
+        }
+
+        public int Factorial(int number)
+        {
+            var fact = 1;
+
+            if(number < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            for(int i = number; i > 0; i--)
+            {
+                fact *= i;
+            }
+
+            return fact;
+        }
+    }
+}
